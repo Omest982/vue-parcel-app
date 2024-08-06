f6f43e11cb72551e3c65a7b8e29280fb83e31c4f
Ivan <omest982@gmail.com>
2024-08-07 01:15:33 +0300

    refactor: refactored navBar

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,17 +1,56 @@
 <script setup>
 import { ref } from "vue";
-import Toolbar from "primevue/toolbar";
 import Button from "primevue/button";
+import Menubar from 'primevue/menubar';
+import Badge from 'primevue/badge';
+
+const items = ref([
+    {
+        label: 'Склад',
+        icon: 'pi pi-home',
+        to: '/'
+    },
+    {
+        label: 'Боксы',
+        icon: 'pi pi-box',
+        to: '/boxes'
+    },
+    {
+        label: 'Посылки',
+        icon: 'pi pi-folder',
+        to: '/parcels'
+    },
+    {
+        label: 'Лицевой счет',
+        icon: 'pi pi-receipt',
+        badge: 3,
+        to: '/account'
+    },
+    {
+        label: 'Личный кабинет',
+        icon: 'pi pi-user',
+        to: '/cabinet'
+    },
+    {
+        label: 'Сообщения',
+        icon: 'pi pi-envelope',
+        to: '/messages'
+    }
+]);
+
 </script>
 
 <template>
-  <Toolbar class="mt-5">
-    <template #start>
-      <router-link to="/"> <Button label="Склад" text /> </router-link>
-      <router-link to="/about"> <Button label="Боксы" text /> </router-link>
-      <Button label="Посылки" text />
-      <Button label="Лицевой счет" badge="2" text />
+  <Menubar :model="items" class="mt-5">
+    <template #item="{ item, props, hasSubmenu, root }">
+      <router-link :to="item.to" v-bind="props.action">
+        <span :class="item.icon" />
+        <span class="ml-2">{{ item.label }}</span>
+        <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
+        <span v-if="item.shortcut" class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{{ item.shortcut }}</span>
+        <i v-if="hasSubmenu" :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
+      </router-link>
     </template>
-  </Toolbar>
+  </Menubar>
 </template>
 
