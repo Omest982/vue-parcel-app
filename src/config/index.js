6c89e175f655882aa3f82e4c9d3c1e245aec1c83
Ivan <omest982@gmail.com>
2024-08-06 18:32:23 +0300

    refactor: changed theme

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import PrimeVue from 'primevue/config';
 
 import 'tailwindcss/tailwind.css'; // Import Tailwind CSS
-import 'primevue/resources/themes/aura-dark-green/theme.css'      
+import 'primevue/resources/themes/aura-light-indigo/theme.css'  // Theme 
 import 'primevue/resources/primevue.min.css'; // Core PrimeVue CSS
 import 'primeicons/primeicons.css'; // PrimeIcons
 
